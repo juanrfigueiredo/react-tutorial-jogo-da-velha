b23ca0cb65c9ecb481c5b5a35159b96794a10ce0
Juan R. Figueiredo <juanrfigueiredo@gmail.com>
2023-10-10 15:46:14 -0300

    primeiro commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+import App from "./App.js";
+import React, { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
+const root = createRoot(document.getElementById("root"));
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
